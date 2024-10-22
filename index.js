edbce7a96a90e8b59b17067b88a2f933bd1847a8
unknown <muhammedalisahin288@gmail.com>
2024-10-22 21:33:50 +0200

    added the final grid size of the futur conyway games

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const trucDeTest = ["chat", "potichat", "chaton", "cat"]
 function makeAGrid(row, column){
     const mainGrid = document.getElementById("grid-button")
     mainGrid.style.display = "grid";
-    mainGrid.style.gridTemplateRows = `repeat(${row}, 1fr)`
-    mainGrid.style.gridTemplateColumns = `repeat(${column}, 1fr)`
+    mainGrid.style.gridTemplateRows = `repeat(${row}, 30px)`
+    mainGrid.style.gridTemplateColumns = `repeat(${column}, 30px)`
+    mainGrid.style.placeContent = `center`;
 
     for(r = 0; r < (row*column); r++) {
             const cell = document.createElement("div")
             cell.style.border = "solid";
-            cell.style.backgroundColor = "#dac1bc";
-            cell.style.paddingRight = "20px";
-            cell.style.paddingLeft = "20px";
-            cell.style.paddingTop = "20px";
-            //cell.innerHTML = "truc"
+            cell.style.backgroundColor = "Grey";
+            cell.style.paddingRight = "5px";
+            cell.style.paddingLeft = "5px";
+            cell.style.paddingTop = "15px";
+            cell.style.paddingBottom = "15px"
             mainGrid.appendChild(cell)
     }
 }
-makeAGrid(100,100)
+makeAGrid(25, 25)
